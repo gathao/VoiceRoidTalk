b35f960c47834a21dc50ea393b52356987697127
gunther <gansa213@gmail.com>
2020-02-08 13:22:50 +0900

    プロジェクト追加

---
+++ SeleniumManager/SeleniumManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace VoiceRoidTalk.VoiceRecognition.ChromeSpeech
+{
+    public class SeleniumManager
+    {
+        private IWebDriver driver = null;
+
+        SeleniumManager()
+        {
+            CreateChromeDriver();
+        }
+
+        public void CreateChromeDriver()
+        {
+            this.driver = new ChromeDriver();
+        }
+
+        public void GotoUrl(string url)
+        {
+            this.driver.Navigate().GoToUrl(url);
+        }
+
+
+    }
+}
